f06077dfdbcbedd620029bf559cd972636142b31
kakavand <kakavandah@gmail.com>
2022-02-15 16:45:51 +0330

    create category page

--- frontend/src/pages/products.page/Products.js
+++ frontend/src/pages/products.page/Products.js
@@ -1,12 +1,44 @@
-import { Header } from 'layout';
-import React from 'react';
+import ProductItem from "components/ProductItem/ProductItem.component";
+import { Header } from "layout";
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getProductsFil } from "redux/action/productFilter.acrion";
+import style from "./product.module.scss";
 
-function Products() {
-  return (
-      <Header>
-          صفحه محصولات
-      </Header>
-  );
+function Products(props) {
+    const params = useParams();
+    const [first, setfirst] = useState([]);
+
+    useEffect(() => {
+        props.gtProductFilter().then((res) => setfirst(res));
+    }, [props]);
+
+    console.log(params.category);
+    return (
+        <Header>
+            <div className={style.container}>
+                <h1>{params.category}</h1>
+                <div className={style.row}>
+                    {first.filter(item => item.category === params.category).map((productItem) => (
+                        <ProductItem
+                            price={productItem.price}
+                            product_name={productItem.firstName}
+                            id={productItem.id}
+                        />
+                    ))}
+                </div>
+            </div>
+        </Header>
+    );
 }
 
-export default Products;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        gtProductFilter: (category) => dispatch(getProductsFil(category)),
+    };
+};
+
+const ProductsRed = connect(null, mapDispatchToProps)(Products);
+
+export default ProductsRed;
